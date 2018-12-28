e7dac2d9e3367e452ef04c432110874b2cc1a906
girovago77 <girovago77@gmail.com>
2018-12-28 18:11:28 +0100

    browsers

---
+++ install_browsers.bash
@@ -0,0 +1,21 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get autoremove
+sudo apt-get autoclean
+sudo apt-get -f install
+
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sudo apt-get autoclean
+sudo apt-get -f install
+
+#Office Libre
+sudo apt-get install libreoffice
+
+#Mozilla Firefox
+sudo apt-get install firefox
+
+#Google Chrome
+sudo apt-get install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome*.deb
+#sudo reboot
+
