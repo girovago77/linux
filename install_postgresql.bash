44fd3f626423cf439d33201c6aafab5c220868b5
girovago77 <46223466+girovago77@users.noreply.github.com>
2018-12-28 16:35:57 +0100

    Update and rename install_4.bash to install_postgresql.bash

---
+++ install_postgresql.bash
@@ -0,0 +1,8 @@
+#http://www.unixmen.com/install-postgresql-9-4-phppgadmin-ubuntu-14-10/
+sudo apt-get install postgresql postgresql-contrib
+#default user and password 
+#sudo -u postgres psql postgres
+#aqui type \password postgres
+#after enter the password twice your user postgres and password are set
+#to create a db with a user and a password : 
+#sudo -u postgres createuser -D -A -P databasename
