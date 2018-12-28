6364f3b9c0a9aa0504db287e3417825f382e4026
girovago77 <46223466+girovago77@users.noreply.github.com>
2018-12-28 16:10:11 +0100

    Update install_2.bash

--- install_2.bash
+++ install_2.bash
@@ -1,15 +1,16 @@
 sudo apt-get update
 sudo apt-get autoremove
-sudo apt-get upgrade
+sudo apt-get upgrade -y 
 sudo apt-get autoremove
+sudo apt-get autoclean
 
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sudo apt-get autoclean
 
-#sudo apt-get install remmina
-#sudo apt-get install simplescreenrecorder
-#sudo apt-get install vpnc network-manager-vpnc
-#sudo apt-get install gnome-control-center
-#sudo apt-get install chromium-browser
+sudo apt-get install remmina
+sudo apt-get install simplescreenrecorder
+sudo apt-get install vpnc network-manager-vpnc
+sudo apt-get install gnome-control-center
+sudo apt-get install chromium-browser
 
 
 ##for Dropbox on Elementary OS
@@ -21,17 +22,17 @@ sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sud
 
 
 ## Sublime Text 2
-#sudo add-apt-repository ppa:webupd8team/sublime-text-2
-#sudo apt-get update
-#sudo apt-get install sublime-text
+sudo add-apt-repository ppa:webupd8team/sublime-text-2
+sudo apt-get update
+sudo apt-get install sublime-text
 
 ## OBS
-#sudo apt-get update && sudo apt-get install ffmpeg 
-#sudo add-apt-repository ppa:obsproject/obs-studio
-#sudo apt-get update && sudo apt-get install obs-studio
+sudo apt-get update && sudo apt-get install ffmpeg 
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt-get update && sudo apt-get install obs-studio
 
 ## Teamviewer
-#wget http://download.teamviewer.com/download/teamviewer_i386.deb
-#sudo dpkg -i teamviewer_i386.deb
-#sudo apt-get install -f
+wget http://download.teamviewer.com/download/teamviewer_i386.deb
+sudo dpkg -i teamviewer_i386.deb
+sudo apt-get install -f
 
